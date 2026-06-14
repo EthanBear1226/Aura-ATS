979a2bf3ef15f6dcdb89b8fc2772289ccda5bbab
EthanBear1226 <15810635212@163.com>
2026-06-14 11:02:19 +0800

    fix: 放开职位基本信息读取接口免登录限制，修复外网投递链接401错误；升级为【复制链接】+【预览】双按钮模式

--- main.py
+++ main.py
@@ -369,7 +369,7 @@ def create_job(job: schemas.JobCreate, db: Session = Depends(get_db), current_us
     return db_job
 
 @app.get("/api/jobs/{job_id}", response_model=schemas.Job)
-def get_job(job_id: int, db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
+def get_job(job_id: int, db: Session = Depends(get_db), current_user: Optional[models.User] = Depends(get_current_user_optional)):
     db_job = db.query(models.Job).filter(models.Job.id == job_id).first()
     if not db_job:
         raise HTTPException(status_code=404, detail="Job not found")
